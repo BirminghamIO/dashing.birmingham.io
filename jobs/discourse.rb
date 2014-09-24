d3c064b92f0482a29074e4a70ebfe50e7e394b2e
Daniel Hollands <daniel@limeblast.co.uk>
2014-09-24 18:56:52 +0100

    minor changes to the discourse stuff

--- jobs/discourse.rb
+++ jobs/discourse.rb
@@ -12,7 +12,7 @@ client = DiscourseApi::Client.new(api_url, api_key, api_user)
 
 SCHEDULER.every '10m', :first_in => 0 do |job|
   latest_topics = client.latest_topics
-  new_topics    = client.new_topics
+  # new_topics    = client.new_topics
 
   if latest_topics
     latest_topics = latest_topics.map do |topic|
@@ -21,10 +21,10 @@ SCHEDULER.every '10m', :first_in => 0 do |job|
     send_event('discourse_latest_topics', items: latest_topics)
   end
 
-  if new_topics
-    new_topics = new_topics.map do |topic|
-      { label: topic['title'], value: topic['posts_count'] }
-    end
-    send_event('discourse_new_topics', items: new_topics)
-  end
+  # if new_topics
+  #   new_topics = new_topics.map do |topic|
+  #     { label: topic['title'], value: topic['posts_count'] }
+  #   end
+  #   send_event('discourse_new_topics', items: new_topics)
+  # end
 end
